38f405bab7ab0c8cfd9ce68c754e8373ecca7229
David Bieber <dbieber@princeton.edu>
2015-01-11 09:27:44 -0800

    more tweets

--- Broca/Process.py
+++ Broca/Process.py
@@ -3,17 +3,27 @@
 def process(data):
     ''' data is a validated JSON-parsed dictionary. Contains all the data
     in BrocaReceiveMessage, in addition to POI, Weather, and Trip history data.
-    
+
     Returns a list of potential posts in SinglePostFormat.
     '''
-    
-    candidate_posts = [MakeSinglePostFormat('yay', data)]
-    
+
+    candidate_posts = []
+
+    if 'POI' in data and 'PictureLoc' in data:
+        candidate_posts.append(MakeSinglePostFormat('Check out my photo at {}!'.format(data['POI']), data))
+
+    if 'POI' in data:
+        candidate_posts.append(MakeSinglePostFormat('Looking forward to a lovely evening at {}'.format(data['POI']), data))
+
+    if 'PictureLoc' in data:
+        candidate_posts.append(MakeSinglePostFormat('Check out my photo!', data))
+
+
     return candidate_posts
 
 def MakeSinglePostFormat(text, data):
     return {
-        'PostText' : text,
+        'PostText' : '{} #BMWJoyRide'.format(text),
         'PictureLoc' : data['PictureLoc'] if 'PictureLoc' in data else '',
         'GPS' : data['PhoneGPS'],
         'PostingService' : ['Twitter'],
